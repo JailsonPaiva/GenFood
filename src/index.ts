918a649bb36c19b30e9df79cbf52d646459df1e5
Jailson Paiva <jailsonp437@gmail.com>
2024-05-31 22:27:28 -0400

    teste rota de login 3

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import express from 'express'
 import { Router, Request, Response } from 'express';
 import { login, callback, updateUser } from './controllers/admin'
 import cookieParser from 'cookie-parser';
+const cors = require('cors');
 
 const PORT = 3333
 const app = express();
 const route = Router()
+app.use(cors());
+
 
 app.use(express.json())
 app.use(cookieParser());
